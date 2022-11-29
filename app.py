9d03a13e4e7a9894ff79cc1e5dd509a3e34ca504
lannyalfiani <lannyalfiani13@gmail.com>
2022-11-29 11:01:58 +0700

    implement redis cache

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy # ORM
-from flask_migrate import Migrate
 from datetime import datetime
 from dotenv import load_dotenv
 from flask_marshmallow import Marshmallow # untuk convert python class instances ke object yg bisa jadi JSON
 from sqlalchemy import desc
-import os
+import os, redis, json
 
 load_dotenv()
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 app = Flask(__name__)
 
@@ -15,15 +16,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:postgres@localhos
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 
-# # untuk connect ke DB
+# untuk connect ke DB
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 ma = Marshmallow(app)
 
 app.secret_key = os.getenv('SECRETKEY')
-
-# Create class Product untuk DB nya (extends db.model), define column + datatype
-class Products(db.Model):
+class Products(db.Model): # Create class Product untuk DB nya (extends db.model), define column + datatype
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String, nullable=False)
   price = db.Column(db.Integer, nullable=False)
@@ -72,46 +70,87 @@ def add_product():
     try:
       newProduct = Products(name, price, description, quantity)
       db.session.add(newProduct)
-      db.session.commit() # untuk commit/save ke DB
+      db.session.commit() # to commit/save to DB
+
+      redis_client.delete("newest_products")
+      redis_client.delete("lowest_products")
+      redis_client.delete("highest_products")
+      redis_client.delete("a_z_products")
+      redis_client.delete("z_a_products")
+
       return jsonify(message=f"Success adding {name} as a new product"), 201
     except Exception as e:
       print(e)
       return jsonify(message=f"Something went wrong! {e}")
 
-#GET products
+#GET products endpointv
 @app.get("/products")
 def fetch_products():
   try:
     if request.args['query'] == 'date_added' and request.args['sort'] == 'newest':
-      products = Products.query.order_by(Products.date_added.desc()).all()
-      product_schema = ProductSchema()
-      products_schema = ProductSchema(many=True)
-      result = products_schema.dump(products)
-      return jsonify(result), 200
+      newest_products_cache = redis_client.get("newest_products")
+      if newest_products_cache is None:
+        products = Products.query.order_by(Products.date_added.desc()).all()
+        product_schema = ProductSchema()
+        products_schema = ProductSchema(many=True)
+        result = products_schema.dump(products)
+        redis_client.set("newest_products", json.dumps(result))
+        return jsonify(result), 200
+      else:
+        return json.loads(newest_products_cache), 200
+
+
     elif request.args['query'] == 'price' and request.args['sort'] == 'highest':
-      products = Products.query.order_by(Products.price.desc()).all()
-      product_schema = ProductSchema()
-      products_schema = ProductSchema(many=True)
-      result = products_schema.dump(products)
-      return jsonify(result), 200
+      highest_products_cache = redis_client.get("highest_products")
+      if highest_products_cache is None:
+        products = Products.query.order_by(Products.price.desc()).all()
+        product_schema = ProductSchema()
+        products_schema = ProductSchema(many=True)
+        result = products_schema.dump(products)
+        redis_client.set("highest_products", json.dumps(result))
+        return jsonify(result), 200
+      else:
+        return json.loads(highest_products_cache), 200
+
     elif request.args['query'] == 'price' and request.args['sort'] == 'lowest':
-      products = Products.query.order_by(Products.price).all()
-      product_schema = ProductSchema()
-      products_schema = ProductSchema(many=True)
-      result = products_schema.dump(products)
-      return jsonify(result), 200
+      lowest_products_cache = redis_client.get("lowest_products")
+      if lowest_products_cache is None:
+        products = Products.query.order_by(Products.price).all()
+        product_schema = ProductSchema()
+        products_schema = ProductSchema(many=True)
+        result = products_schema.dump(products)
+        redis_client.set("lowest_products", json.dumps(result))
+        return jsonify(result), 200
+      else:
+        return json.loads(lowest_products_cache), 200
+
+
     elif request.args['query'] == 'name'and request.args['sort'] == 'a-z':
-      products = Products.query.order_by(Products.name).all()
-      product_schema = ProductSchema()
-      products_schema = ProductSchema(many=True)
-      result = products_schema.dump(products)
-      return jsonify(result), 200
+      a_z_cache = redis_client.get("a_z_products")
+
+      if a_z_cache is None:
+        products = Products.query.order_by(Products.name).all()
+        product_schema = ProductSchema()
+        products_schema = ProductSchema(many=True)
+        result = products_schema.dump(products)
+        redis_client.set("a_z_products", json.dumps(result))
+        return jsonify(result), 200
+      else:
+        return json.loads(a_z_cache), 200
+
     elif request.args['query'] == 'name' and request.args['sort'] == 'z-a':
-      products = Products.query.order_by(Products.name.desc()).all()
-      product_schema = ProductSchema()
-      products_schema = ProductSchema(many=True)
-      result = products_schema.dump(products)
-      return jsonify(result), 200
+      z_a_cache = redis_client.get("z_a_products")
+
+      if z_a_cache is None:
+        products = Products.query.order_by(Products.name.desc()).all()
+        product_schema = ProductSchema()
+        products_schema = ProductSchema(many=True)
+        result = products_schema.dump(products)
+        redis_client.set("z_a_products", json.dumps(result))
+        return jsonify(result), 200
+      else:
+        return json.loads(z_a_cache), 200
+
     else:
       return jsonify(message='Bad request, query string is not recognised!'), 400
   except Exception as error:
